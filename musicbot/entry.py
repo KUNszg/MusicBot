dac9dff568be7d6cf3655d35c29d9cd8f4cae7b5
KUNszg <dev@kunszg.com>
2024-12-16 21:58:11 +0100

    revert changes

--- musicbot/entry.py
+++ musicbot/entry.py
@@ -611,7 +611,7 @@ class URLPlaylistEntry(BasePlaylistEntry):
             input_file,
             "-af",
             "loudnorm=I=-24.0:LRA=7.0:TP=-2.0:linear=true:print_format=json",
-            "-S vcodec:h264,res:720,acodec:aac",
+            "-f",
             "null",
             "/dev/null",
             "-hide_banner",
