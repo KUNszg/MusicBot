94408bdbd6bdd711fcd510acfd29cb15201ee9e6
KUNszg <dev@kunszg.com>
2024-12-16 22:12:30 +0100

    skip playlists

--- musicbot/downloader.py
+++ musicbot/downloader.py
@@ -57,6 +57,7 @@ ytdl_format_options_immutable = MappingProxyType(
         "source_address": "0.0.0.0",
         "usenetrc": True,
         "no_color": True,
+        "match_filter": lambda info_dict: None if not info_dict.get('is_playlist') else 'Skipping playlists'
     }
 )
 
